d06c55598b299bffc55a9ce980e228ce548a2ee8
Rail <well016@mail.ru>
2025-07-30 23:45:27 +0300

    инициализация

---
+++ test1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+void replace(char * dst, const char * src)
+{
+    size_t lenbomb = strlen("bomb");
+    size_t lenwater = strlen("watermelon");
+
+
+    char * s;   // src: указатель на начало подстроки, где ищем bomb
+    char * p;   // src: указатель на начало bomb
+    char * d;   // dst: указатель на конец уже скопированной части
+
+
+    // сначала
+    s = (char *)src;            // ищем во всей строке src
+    p = NULL;                   // про бомбу ничего не известно
+    d = dst;                    // еще ничего не скопировали
+
+
+    // ищем бомбу начиная с адреса s
+    p = strstr(s, "bomb");      // p указывает на начало бомбы или NULL
+
+
+    // обезвредим бомбу
+     while ((p = strstr(s, "bomb")) != NULL) {            // бомба есть
+        size_t n = p - s;       // количество символов перед бомбой
+        strncpy(d, s, n);       // скопируем символы перед бомбой в dst
+        d = d + n;              // передвинем d в конце строки
+        strcpy(d, "watermelon");// допишем в конец строки арбуз
+        d = d + lenwater;       // передвинем конец строки за арбуз
+
+
+        s = p + lenbomb;        // указатель на остаток строки поставим за бомбой
+    }
+
+
+    // оставшаяся строка
+    strcpy(d, s);
+}
+
+int main()
+{
+    char d[1000];
+    char out[1000];
+    while (fgets(d, sizeof(d), stdin) != NULL) {
+        // Удаляем символ новой строки в конце, если он есть
+        size_t len = strlen(d);
+        if (len > 0 && d[len-1] == '\n') {
+            d[len-1] = '\0';
+        }
+        replace(out, d);
+        printf("%s\n", out);
+    }
+    return 0;
+}   
